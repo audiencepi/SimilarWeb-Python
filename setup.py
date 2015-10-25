1601d65861c39a0389a6066f2371ad1e9361d837
Ryan Liao <pirsquare.ryan@gmail.com>
2015-10-25 17:21:43 +0800

    Update setup configuration

--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
 from setuptools import find_packages
 from setuptools import setup
+import io
+import os
 
 VERSION = '0.0.2'
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(fpath(filename), encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+def get_requirements():
+    return [line.rstrip('\n') for line in open(fpath('requirements.txt'))]
+
 
 setup_args = dict(
     name='SimilarWeb-Python',
@@ -9,9 +30,9 @@ setup_args = dict(
     url='https://github.com/audiencepi/SimilarWeb-Python',
     version=VERSION,
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    install_requires=["requests", "tldextract"],
+    install_requires=get_requirements(),
     author='Ryan Liao',
     author_email='pirsquare.ryan@gmail.com',
     classifiers=[
